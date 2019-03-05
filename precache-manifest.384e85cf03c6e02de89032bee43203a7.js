cf399f15581e6e6b108aeb39dd89e1b482756d28
graphicskart <graphicskart2018@gmail.com>
2019-03-05 16:32:59 +0530

    Updates

---
+++ precache-manifest.384e85cf03c6e02de89032bee43203a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31754efba32a678f6e66",
+    "url": "/bootstrap-modal-auth2/static/js/runtime~main.31754efb.js"
+  },
+  {
+    "revision": "eac4a37bfaf8d2b53dec",
+    "url": "/bootstrap-modal-auth2/static/js/main.eac4a37b.chunk.js"
+  },
+  {
+    "revision": "1fa70b92b08fb5a58565",
+    "url": "/bootstrap-modal-auth2/static/js/2.1fa70b92.chunk.js"
+  },
+  {
+    "revision": "eac4a37bfaf8d2b53dec",
+    "url": "/bootstrap-modal-auth2/static/css/main.d86132c1.chunk.css"
+  },
+  {
+    "revision": "daad9182b3a649faa4e20ec8c9b42391",
+    "url": "/bootstrap-modal-auth2/index.html"
+  }
+];
